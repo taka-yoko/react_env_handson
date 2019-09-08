2c6d110861bdddd01c1a522e725580dc08520568
taka-yoko <*****@users.noreply.github.com>
2019-09-08 21:54:10 +0900

    finished step5

---
+++ src/stories/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import SimpleButton from "../index";
+
+storiesOf("Button", module).add("with text", () => (
+  <SimpleButton onClick={action("clicked")}>Hello Button</SimpleButton>
+));

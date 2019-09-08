f2eee6c9ddd6d2ee8bd9bcee178c415b6bd032f6
taka-yoko <*****@users.noreply.github.com>
2019-09-08 23:26:18 +0900

    webアプリの開発設定

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import SimpleButton from "../index";
+// import SimpleButton from "../index";
 
-storiesOf("Button", module).add("with text", () => (
-  <SimpleButton onClick={action("clicked")}>Hello Button</SimpleButton>
-));
+// storiesOf("Button", module).add("with text", () => (
+//   <SimpleButton onClick={action("clicked")}>Hello Button</SimpleButton>
+// ));
